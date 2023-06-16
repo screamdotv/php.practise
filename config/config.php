5de8592ec311ad47869e17bb1b57f6bf2577fa12
Vlad Krikun <krik.vladislav@gmail.com>
2023-06-16 10:42:04 +0300

    create and configurate file route

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+define("ROOT", dirname(__DIR__)); // "D:\programs\OSPanel\domains\php.practise"
+define("PUBLIC", ROOT . '/public');
+define("CONFIG", ROOT . '/config');
+define("CORE", ROOT . '/core');
+define("APP", ROOT . '/app');
+define("CONTROLLERS", APP . '/controllers');
+define("VIEWS", APP . '/views');
+define("PATH", 'http://php.practise');
--- public/index.php
+++ public/index.php
@@ -1,25 +1,10 @@
 <?php
+// File front controller.
 
-define("ROOT", dirname(__DIR__)); // "D:\programs\OSPanel\domains\php.practise"
-define("PUBLIC", ROOT . '/public');
-define("CORE", ROOT . '/core');
-define("APP", ROOT . '/app');
-define("CONTROLLERS", APP . '/controllers');
-define("VIEWS", APP . '/views');
-define("PATH", 'http://php.practise');
-
+require_once dirname(__DIR__) . '/config/config.php';
 
 require_once CORE . '/functions.php';
 
-$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+require_once CORE . '/route.php';
 
-if ($uri === ''){
-    require_once CONTROLLERS . '/index.php';
-} elseif ($uri == 'about') {
-    require_once CONTROLLERS . '/about.php';
-} elseif ($uri == 'post') {
-    
-} else {
-    abort();
-}
 
